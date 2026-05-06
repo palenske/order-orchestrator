4485434fd04c0f7c80f097c74fccb30934c62bca
palenske <viniciuspalenske@gmail.com>
2026-05-06 18:04:30 -0300

    fix Prisma 7 config and add dotenv

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -7,7 +7,10 @@ import {
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   private readonly logger = new Logger(PrismaService.name);
 
   async onModuleInit() {
